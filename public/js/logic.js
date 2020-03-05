03cdb562c561302fa821b68806bce5145b38bc01
Spaghetti Jones <connor.breault@comcast.net>
2020-03-05 15:06:15 -0800

    boilerplate added

---
+++ public/js/logic.js
@@ -0,0 +1,3 @@
+$("#Hi").on("click", function () {
+    alert("hi")
+})
---
+++ server.js
@@ -0,0 +1,43 @@
+require("dotenv").config();
+var express = require("express");
+var exphbs = require("express-handlebars");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// Handlebars
+app.engine(
+    "handlebars",
+    exphbs({
+        defaultLayout: "main"
+    })
+);
+app.set("view engine", "handlebars");
+
+require("./routes/htmlRoutes")(app);
+
+// var syncOptions = { force: false };
+
+// If running a test, set syncOptions.force to true
+// clearing the `testdb`
+if (process.env.NODE_ENV === "test") {
+    syncOptions.force = true;
+}
+
+app.get('/', (req, res) => res.render('index')); ``
+
+
+app.listen(PORT, function () {
+    console.log(
+        "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+        PORT,
+        PORT
+    );
+});
+
+module.exports = app;
