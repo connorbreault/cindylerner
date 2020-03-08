97b2cec1842a3c19e2ed3868a7dad6a5fb40aa78
Spaghetti Jones <connor.breault@comcast.net>
2020-03-08 03:13:19 -0700

    priorities, meet cindy, from campaign sections added to index

--- public/js/logic.js
+++ public/js/logic.js
@@ -1,5 +1,18 @@
-$("#bottomDonateButton").on("click", function () {
-    let newVolunteerEmail = $("#bottomDonateEmail").val()
-    let newVolunteerZip = $("#bottomDonateZip").val()
+// INDEX GET INVOLVED BUTTONS
+$("#topButtonDesktopIndex").on("click", function () {
+    let newVolunteerEmail = $("#topEmailDesktopIndex").val()
+    let newVolunteerZip = $("#topZipDesktopIndex").val()
+    alert(newVolunteerEmail + newVolunteerZip)
+})
+$("#topButtonResponsiveIndex").on("click", function () {
+    let newVolunteerEmail = $("#topEmailResponsiveIndex").val()
+    let newVolunteerZip = $("#topZipResponsiveIndex").val()
+    alert(newVolunteerEmail + newVolunteerZip)
+})
+
+
+$("#bottomInvolveButton").on("click", function () {
+    let newVolunteerEmail = $("#bottomInvolveEmail").val()
+    let newVolunteerZip = $("#bottomInvolveZip").val()
     alert(newVolunteerEmail + newVolunteerZip)
 })
