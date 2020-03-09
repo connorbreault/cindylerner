e9b7f5d1f26612e72891e9a64a120877600ae7c5
Spaghetti Jones <connor.breault@comcast.net>
2020-03-08 22:22:09 -0700

    custom beach images added

--- public/js/logic.js
+++ public/js/logic.js
@@ -1,18 +1,32 @@
-// INDEX GET INVOLVED BUTTONS
-$("#topButtonDesktopIndex").on("click", function () {
-    let newVolunteerEmail = $("#topEmailDesktopIndex").val()
-    let newVolunteerZip = $("#topZipDesktopIndex").val()
-    alert(newVolunteerEmail + newVolunteerZip)
-})
-$("#topButtonResponsiveIndex").on("click", function () {
-    let newVolunteerEmail = $("#topEmailResponsiveIndex").val()
-    let newVolunteerZip = $("#topZipResponsiveIndex").val()
-    alert(newVolunteerEmail + newVolunteerZip)
-})
+$(document).ready(function () {
 
+    // GET INVOLVED BUTTONS
+    $("#topButtonDesktopIndex").on("click", function () {
+        let email = $("#topEmailDesktopIndex").val()
+        let zip = $("#topZipDesktopIndex").val()
+        addVolunteer(email, zip)
+    })
+    $("#topButtonResponsiveIndex").on("click", function () {
+        let email = $("#topEmailResponsiveIndex").val()
+        let zip = $("#topZipResponsiveIndex").val()
+        addVolunteer(email, zip)
+    })
+    $("#bottomInvolveButton").on("click", function () {
+        let email = $("#bottomInvolveEmail").val()
+        let zip = $("#bottomInvolveZip").val()
+        addVolunteer(email, zip)
+    })
 
-$("#bottomInvolveButton").on("click", function () {
-    let newVolunteerEmail = $("#bottomInvolveEmail").val()
-    let newVolunteerZip = $("#bottomInvolveZip").val()
-    alert(newVolunteerEmail + newVolunteerZip)
-})
+    // ADD VOLUNTEER TO FIREBASE
+    function addVolunteer(email, zip) {
+        if (email === "" | zip === "") {
+            alert("Please fill out both inputs")
+        } else {
+            let newVolunteer = {
+                email: email,
+                zip: zip
+            }
+            // Add to DB
+        }
+    }
+});
